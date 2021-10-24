d5ad23682c532a17bf90b3832b3f97d6621235f2
Lois Tatis <tatislois@icloud.com>
2021-10-24 12:18:46 -0500

    add eslint action

---
+++ src/NewComponent.js
@@ -0,0 +1,10 @@
+function NewComponent() {
+  console.log("Original Component warning");
+  return (
+    <div className="App">
+      <p>New Component</p>
+    </div>
+  );
+}
+
+export default NewComponent;
